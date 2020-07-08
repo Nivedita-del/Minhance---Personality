69b2f12a96d140ac2fb613d9c4463786a80f7ec9
Nivedita-del <niveditamadhava1@gmail.com>
2020-07-08 15:46:04 +0530

    php

--- dist/PHP-Database-connection-master/db.php
+++ dist/PHP-Database-connection-master/db.php
@@ -7,7 +7,7 @@ $conn=mysql_connect($servername,$username,$password);
 if(!$conn)
   echo "Error in Connection".mysql_error();
 
-$dbcheck=mysql_select_db("lname");
+$dbcheck=mysql_select_db("temp");
 if(!$dbcheck)
 	echo "Error selecting Database<br>".mysql_error();
 //else echo "Success";
